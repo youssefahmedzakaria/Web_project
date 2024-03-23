d4cbb0469010a23ae04ae5a6f8c7e4d1800ce366
youssef ahmed <100100252+youssefahmedzakaria@users.noreply.github.com>
2024-03-23 18:22:03 +0200

    Add files via upload

---
+++ DB_Ops.php
@@ -0,0 +1,53 @@
+<?php
+$hostName = "localhost";
+$DBusername = "root";
+$DBpassword = "";
+$DBname = "login_register";
+
+$conn = mysqli_connect($hostName, $DBusername, $DBpassword, $DBname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+function register($fullname, $username, $dob, $phone, $address, $passwordHash, $image, $email) {
+    global $conn;
+    $sql = "INSERT INTO users (fullname, username, DOB, phone, address, password, image, email) VALUES ('$fullname', '$username', '$dob', '$phone', '$address', '$passwordHash', '$image', '$email')";
+    if (mysqli_query($conn, $sql)) {
+        echo "<div class='alert alert-success'> Registration successful </div>";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
+
+function checkUsername($username) {
+    global $conn;
+    $sql = "SELECT * FROM users WHERE username = '$username'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function checkEmail($email) {
+    global $conn;
+    $sql = "SELECT * FROM users WHERE email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function checkPhone($phone) {
+    global $conn;
+    $sql = "SELECT * FROM users WHERE phone = '$phone'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}    
+?>
---
+++ registration.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Home</title>
+</head>
+
+<body>
+    <div class="container">
+        <form action = "registration.php" method = "post">
+            <?php 
+            if (isset($_POST['submit'])) {
+                $fullname = $_POST['fullname'];
+                $username = $_POST['username'];
+                $dob = $_POST['dob'];
+                $phone = $_POST['phone'];
+                $address = $_POST['address'];
+                $password = $_POST['password'];
+                $cpassword = $_POST['cpassword'];
+                $image = $_POST['image'];
+                $email = $_POST['email'];
+                $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+
+                $errors = array();
+                if (empty($fullname) or empty($username) or empty($dob) or
+                 empty($phone) or empty($address) or empty($password) or 
+                 empty($cpassword) or empty($image) or empty($email)){
+                    array_push($errors, "All fields are required");
+                }
+
+                if ($password != $cpassword){
+                    array_push($errors, "Password do not match");
+                }
+
+                if(strlen($password) < 8){
+                    array_push($errors, "Password must be at least 6 characters");
+                }
+
+                if (!preg_match("/[0-9]/", $password)) {
+                    array_push($errors, "Password must contain at least one number");
+                }
+
+                if (!preg_match("/[^a-zA-Z0-9]/", $password)) {
+                    array_push($errors, "Password must contain at least one special character");
+                }
+
+                if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                    array_push($errors, "Email is invalid");
+                }
+                require_once "DB_Ops.php";
+                if (count($errors) >0){
+                    foreach($errors as $error){
+                        echo "<div class = 'alert alert-danger'> $error </div>";
+                    }
+                }
+                else if(checkUsername($username)){
+                    echo "<div class = 'alert alert-danger'> Username already exists </div>";
+                }
+                else if(checkEmail($email)){
+                    echo "<div class = 'alert alert-danger'> Email already exists </div>";
+                }
+                else if(checkPhone($phone)){
+                    echo "<div class = 'alert alert-danger'> Phone number already exists </div>";
+                }
+                else{
+                    require_once "DB_Ops.php";
+                    register($fullname, $username, $dob, $phone, $address, $passwordHash, $image, $email);
+                    header("Location: index.php");
+                }
+            }
+
+            ?>
+            <div class="form-group">
+                <!-- full name  -->
+               <input type="text" class = "form-control" name="fullname" placeholder = "Full Name">
+            </div>
+            <div class="form-group">
+                <!-- user name  -->
+               <input type="text"class = "form-control" name="username" placeholder = "userName">
+            </div>
+            <div class="form-group">
+                <!-- date of birth  -->
+               <input type= "date"class = "form-control" name="dob" placeholder = "Date of Birth">
+            </div>
+            <div class="form-group">
+                <!-- phone  -->
+               <input type= "tel" class = "form-control"name="phone" placeholder = "Phone">
+            </div>
+            <div class="form-group">
+                <!-- adress  -->
+               <input type= "text"class = "form-control" name="address" placeholder = "Address">
+            </div>
+            <div class="form-group">
+                <!-- password  -->
+                <!-- and validate the password -->
+               <input type= "password" class = "form-control"name="password" placeholder = "Password">
+            </div>
+            <div class="form-group">
+                <!-- confirm password  -->
+               <input type= "password" class = "form-control"name="cpassword" placeholder = "Confirm Password">
+            </div>
+            <div class="form-group">
+                <!-- user Image  -->
+                <input type= "file" class = "form-control"name="image" placeholder = "Image">
+            </div>
+            <div class="form-group">
+                <!-- email  -->
+                <input type= "email" class = "form-control"name="email" placeholder = "Email">
+            </div>
+            <div class="form-btn">
+                <!-- submit button  -->
+                <button type="submit" class ="btn btn-primary"  name="submit">Submit</button>
+            </div>
+            
+
+</body>
+</html>
