681ca7d3d7a44619b1a457ee08bab834d9946b13
youssefahmedzakaria <youssefahmed052@gmail.com>
2024-03-26 06:49:57 +0200

    Minor edits in login style

--- login.php
+++ login.php
@@ -28,13 +28,9 @@ if(isset($_POST['submit'])){
 </head>
 <body>
 <?php include 'header.php';?>
-        <div class="row">
+        <div class="container">
             <div class="col-md-3 offset-md-3">
-                <div class="card">
-                    <div class="card-header">
-                        Login
                     </div>
-                    <div class="card-body">
                         <form action="login.php" method="post">
                             <div class="form-group">
                             <label for="username">Username</label>
@@ -47,9 +43,6 @@ if(isset($_POST['submit'])){
                             <button type="submit" class="btn btn-primary">Login</button>
                         </form>
                     </div>
-            </div>
-        </div>
-    </div>
     <?php include 'footer.php';?>
 
 </body>
